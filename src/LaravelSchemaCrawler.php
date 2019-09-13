e1103867d80e085f4f25e6a245010ee9fac94956
Daniel Werner <vernerd@gmail.com>
2019-09-13 22:03:46 +0200

    Format

--- src/LaravelSchemaCrawler.php
+++ src/LaravelSchemaCrawler.php
@@ -9,7 +9,11 @@ class LaravelSchemaCrawler
 
         $dbDefault = config('database.default');
         $dbConnection = config('database.' . $dbDefault);
-        $url = sprintf('jdbc:%1$s://%2$s:%3$s?serverTimezone=UTC', $dbConnection['driver'], $dbConnection['host'], $dbConnection['port']);
+
+        $url = sprintf('jdbc:%1$s://%2$s:%3$s?serverTimezone=UTC',
+            $dbConnection['driver'],
+            $dbConnection['host'],
+            $dbConnection['port']);
 
         $command = sprintf(
             './bin/schemacrawler/schemacrawler.sh --user=%4$s --password=%5$s --info-level=standard --command=schema --url="%2$s" --output-file="%1$s" --output-format=pdf --schemas=%3$s',
@@ -18,7 +22,7 @@ class LaravelSchemaCrawler
             $dbConnection['database'],
             $dbConnection['username'],
             $dbConnection['password']
-            );
+        );
 
         exec($command);
     }
